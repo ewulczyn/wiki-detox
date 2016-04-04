8ac10d91c775412a24e7c4bda0add090676e8946
Ellery Wulczyn <ellerywulczyn@Ellery-Wulczyns-MacBook-Pro.local>
2016-04-04 13:35:33 -0700

    commiting notebook on prolific blocked users

--- wikipedia/src/diff_utils.py
+++ wikipedia/src/diff_utils.py
@@ -1,11 +1,79 @@
-try:
-  from bs4 import BeautifulSoup
-  import mwparserfromhell
-except:
-  print('Could not import bs4 or mwparserfromhell')
-  pass
+from bs4 import BeautifulSoup
+import  mwparserfromhell
 import re
 import pandas as pd
+import copy
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ]
+
+
+month_or = '|'.join(months)
+date_p = re.compile('\d\d:\d\d, \d?\d (%s) \d\d\d\d \(UTC\)' % month_or)
+    
+def remove_date(comment):
+    return re.sub(date_p , '', comment )
+
+
+
+pre_sub_patterns = [
+                    ('\[\[Image:.*?\]\]', ''),
+                    ('<!-- {{blocked}} -->', ''),
+                    ('NEWLINE', '\n'),
+                    ('\[\[File:.*?\]\]', ''),
+                    ('\[\[User:.*?\|.*?\]\]', ''),
+                    ('\(\[\[User talk:.*?\|talk\]\]\)', ''),
+                   ]
+
+post_sub_patterns = [
+                    ('--', ''),
+                    (' :', ' '),
+                    ]
+
+def substitute_patterns(s, sub_patterns):
+    for p, r in sub_patterns:
+        s = re.sub(p, r, s)
+    return s
+
+def strip_html(s):
+    s = BeautifulSoup(s, 'html.parser').get_text()
+    return s
+
+def strip_mw(comment):
+    return mwparserfromhell.parse(comment).strip_code()
+
+
+def clean(df):
+    df = copy.deepcopy(df)
+    df.rename(columns = {'insertion': 'diff'}, inplace = True)
+    df.dropna(subset = ['diff'], inplace = True)
+    df['clean_diff'] = df['diff']
+    
+    df['clean_diff'] = df['clean_diff'].apply(remove_date)
+    df['clean_diff'] = df['clean_diff'].apply(lambda x: substitute_patterns(x, pre_sub_patterns))
+    df['clean_diff'] = df['clean_diff'].apply(strip_mw)
+    df['clean_diff'] = df['clean_diff'].apply(strip_html)
+    df['clean_diff'] = df['clean_diff'].apply(lambda x: substitute_patterns(x, post_sub_patterns))
+
+
+    try:
+        del df['rank']
+    except:
+        pass
+    df.dropna(subset = ['clean_diff'], inplace = True)
+    df = df[df['clean_diff'] != '']
+    return df
+
 
 
 def show_comments(d, n = 10):
@@ -15,7 +83,25 @@ def show_comments(d, n = 10):
         print(r['clean_diff'])
         print('\n\n', '#' * 80, '\n\n')
 
-### Strip Admin Messages
+def find_pattern(d, pattern, column):
+    p = re.compile(pattern)
+    return d[d[column].apply(lambda x: p.search(x) is not None)]
+
+def exclude_pattern(d, pattern, column):
+    p = re.compile(pattern)
+    return d[ d[column].apply(lambda x: p.search(x) is None)]
+
+def exclude_few_tokens(d, n):
+    return d[d['clean_diff'].apply(lambda x: len(x.split(' ')) > n)]
+
+def exclude_short_strings(d, n):
+    return d[d['clean_diff'].apply(lambda x: len(x) > n)]  
+
+def remove_admin(d, patterns):
+    d_reduced = copy.deepcopy(d)
+    for pattern in patterns:
+        d_reduced = exclude_pattern(d_reduced, pattern, 'diff')
+    return d_reduced
 
 patterns =[
     '\[\[Image:Octagon-warning',
@@ -29,6 +115,9 @@ patterns =[
     '\[\[File:Ambox',
     '\[\[File:Nuvola',
     '\[\[File:Stop',
+    '\[\[File:Copyright-problem',
+    '\[\[File:Copyright-problem',
+    '\[\[File:Copyright-problem',
     '\[\[File:Copyright-problem',
     '\|alt=Warning icon\]\]',
     'The article .* has been \[\[Wikipedia:Proposed deletion\|proposed for deletion\]\]',
@@ -44,146 +133,41 @@ patterns =[
     '== 5 Million: We celebrate your contribution ==',
     ]
 
-compiled_patterns = {p:re.compile(p) for p in patterns}
 
-
-def find_pattern_df(df,col, pattern):
-    p = re.compile(pattern)
-    return df[df[col].apply(lambda x: p.search(x) is not None)]
-
-def has_pattern(col, pattern):
-    return lambda x: compiled_patterns[pattern].search(x[col]) is not None
-
-
-
-def exclude_pattern_df(df, col, pattern):
-    p = re.compile(pattern)
-    return df[df[col].apply(lambda x: p.search(x) is None)]
-
-def does_not_have_pattern(col, pattern):
-    return lambda x: compiled_patterns[pattern].search(x[col]) is None
-
-
-
-def exclude_few_tokens_df(df, col, min_tokens):
-    return df[df[col].apply(lambda x: len(x.split(' ')) >= min_tokens)]
-
-def has_enough_tokens(col, min_tokens):
-    return lambda x: len(x[col].split(' ')) >= min_tokens
-
-
-def exclude_short_strings_df(df, col, min_chars):
-    return df[df[col].apply(lambda x: len(x) >= min_chars)]  
-
-def has_enough_chars(col, min_chars):
-    return lambda x: len(x[col]) >= min_chars
-
-
-def remove_admin_df(df, patterns):
-    d_reduced = copy.deepcopy(df)
-    for pattern in patterns:
-        d_reduced = exclude_pattern(d_reduced, pattern, 'diff')
-    return d_reduced
-
-def no_patterns(col, patterns):
-    def f(x):
-        fs  = [has_pattern(col, p) for p in patterns]
-        return sum([f(x) for f in fs]) == 0
-    return f 
-
-
-
-### Clean up Markup ###
+def print_block_data(r):
+    block_reasons = r['block_reasons'].split('PIPE')
+    block_timestamps = r['block_timestamps'].split('PIPE')
+    block_actions = r['block_actions'].split('PIPE')
+    block_params = r['block_params'].split('PIPE')
     
-def remove_date(comment):
+    for i in range(len(block_reasons)):
+        print('Log Event #: ', i+1)
+        print('Action:',block_actions[i] )
+        print('Time:', block_timestamps[i] )
+        print('Reason:',block_reasons[i] )
+        print('Parameters:',block_params[i] )
 
-    months = ['January',
-          'February',
-          'March',
-          'April',
-          'June',
-          'July',
-          'August',
-          'September',
-          'October',
-          'November',
-          'December',
-        ]
-
-
-    month_or = '|'.join(months)
-    date_p = re.compile('\d\d:\d\d, \d?\d (%s) \d\d\d\d \(UTC\)' % month_or)
-    return re.sub(date_p , '', comment )
-
-
-
-pre_sub_patterns = [
-                      ('\[\[Image:.*?\]\]', ''),
-                      ('<!-- {{blocked}} -->', ''),
-                      ('NEWLINE', '\n'),
-                      ('\[\[File:.*?\]\]', ''),
-                      ('\[\[User:.*?\|.*?\]\]', ''),
-                      ('\(\[\[User talk:.*?\|talk\]\]\)', ''),
-                   ]
-
-post_sub_patterns = [
-                      ('--', ''),
-                      (' :', ' '),
-                    ]
-
-def substitute_patterns(s, sub_patterns):
-    for p, r in sub_patterns:
-        s = re.sub(p, r, s)
-    return s
-
-def strip_html(comment):
-    return BeautifulSoup(comment, 'html.parser').get_text()
-
-def strip_mw(comment):
-    return mwparserfromhell.parse(comment).strip_code()
-
-
-def clean(df):
-    df = copy.deepcopy(df)
-    df.dropna(subset = ['insertion'], inplace = True)
-    col = 'clean_insertion'
-    df[col] = df['insertion']
-    df[col] = df[col].apply(remove_date)
-    df[col] = df[col].apply(lambda x: substitute_patterns(x, pre_sub_patterns))
-    df[col] = df[col].apply(strip_mw)
-    df[col] = df[col].apply(strip_html)
-    df[col] = df[col].apply(lambda x: substitute_patterns(x, post_sub_patterns))
-    df.dropna(subset = [col], inplace = True)
-    df = df[df[col] != '']
-    return df
-
-
-def apply_field_modifier(f, col):
-    def mod(x):
-      x[col] = f(x[col])
-      return x
-    return mod
-
-def apply_complex_field_modifier(f, col, aux):
-    def mod(x):
-      x[col] = f(x[col], aux)
-      return x
-    return mod
-
-
-def clean_rdd(rdd):
-  col = 'clean_insertion'
-
-  def copy_insertion(x):
-    x[col] = x['insertion']
-
-  return rdd.filter(lambda x: x['insertion'] is not None and len(x['insertion']) > 0) \
-           .map(copy_insertion) \
-           .map(apply_field_modifier(remove_date, col)) \
-           .map(apply_complex_field_modifier(substitute_patterns, col, pre_sub_patterns)) \
-           .map(apply_field_modifier(strip_mw, col)) \
-           .map(apply_field_modifier(strip_html, col)) \
-           .map(apply_complex_field_modifier(substitute_patterns, col, post_sub_patterns)) \
-           .filter(lambda x: x[col] is not None and len(x[col]) > 0)
-
-
+def print_user_history(d, user):
+    
+    """
+    Print out users comments in order
+    """
+    d = d.fillna('')
+    
+    d_u = d[d['user_text'] == user].sort_values(by ='rev_timestamp')
+    
+    print ('#' * 80)
+    print('History for user: ', user)
+    print('\nBlock History')
+    print_block_data(d_u.iloc[0])
+    print ('#' * 80)
+    
+    
+    for i, r in d_u.iterrows():
+        print ('\n')
+        print ('User: ', r['user_text'])
+        print ('User Talk Page: ', r['page_title'])
+        print('Timestamp: ', r['rev_timestamp'])
+        print ('\n')
+        print (r['clean_diff'])
+        print ('_' * 80)
