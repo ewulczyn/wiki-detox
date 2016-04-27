e2cf302cb8402d26df73d039d5cc8ccf1991e6ee
Ellery Wulczyn <ellerywulczyn@Ellery-Wulczyns-MacBook-Pro.local>
2016-04-27 14:59:34 -0700

    new models

--- wikipedia/src/mwdiff/mwdiff.sh
+++ wikipedia/src/mwdiff/mwdiff.sh
@@ -28,3 +28,16 @@ tsv_dir=/srv/public-datasets/enwiki/${name}_diffs_tsv
 nice python ~/mwdiffs_to_tsv.py \
 --path_glob ${json_dir}/enwiki-${day}-pages-meta-history\*.bz2  \
 --output_dir ${tsv_dir}
+
+
+rsync -avzh stat1003.eqiad.wmnet:/srv/public-datasets/enwiki/article_talk_diffs_tsv ~/talk_page_abuse/wikipedia/data
+
+rsync -avzh ~/talk_page_abuse/wikipedia/data/article_talk_diffs_tsv stat1002.eqiad.wmnet:/home/ellery/talk_page_abuse/wikipedia/data
+
+cd ~/talk_page_abuse/wikipedia/data/article_talk_diffs_tsv
+
+bzip2 -d *.bz2
+
+cd ..
+
+hadoop fs -copyFromLocal article_talk_diffs_tsv /user/ellery/article_talk_diffs_tsv
