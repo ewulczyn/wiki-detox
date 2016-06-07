a777085a426305579343a95b570e2d737aa43730
Ellery Wulczyn <ellerywulczyn@Ellery-Wulczyns-MacBook-Pro.local>
2016-06-06 23:13:10 -0700

    updated app

--- app/app.py
+++ app/app.py
@@ -14,35 +14,66 @@ app.debug = True
 def main():
     return render_template('index.html')
 
+def process_rev_id(request):
+    ret = {} 
+    text = get_comment(request.args['input'])
+
+    if not text:
+        ret['error'] = 'ERROR: invalid revision id'
+    else:
+        ret['text'] = text
+        ret['p'] = apply_model(text, request)
+    return ret
+
+
+def process_text(request):
+    ret = {} 
+    text = request.args['input']
+    ret['p'] = apply_model(text, request)
+    return ret
+    
+
+def apply_model(text, request):
+    mtype = request.args['model']
+    model = model_data[mtype]['model']
+    classes = model_data[mtype]['classes']
+    probs = model.predict_proba([text])[0]
+    probs = ['%0.2f' % p for p in probs]
+    return list(zip(classes, probs))
+    
+
+def validate_params(request):
+
+    if request.args.get('model', '') not in model_data.keys():
+        return 'ERROR: model type not provided'
+    elif 'input_type' not in request.args:
+        return 'ERROR: input_type not provided'
+    elif request.args['input_type'] not in ['rev_id', 'text']:
+        return 'ERROR: input_type must be rev_id or text'
+    elif 'input' not in request.args:
+        return 'ERROR: model input not provided'
+    elif len(request.args['input']) == 0:
+        return 'ERROR: empty input'
+    else:
+        return None
+
+
 
 @app.route('/api')
 def get_insult_scores():
     print(request.args)
-    t1 = time.time()
 
-    assert(request.args.get('model', '') in model_data.keys())
-    assert('text' in request.args or 'rev_id' in request.args)
+    validation_error = validate_params(request)
 
-    mtype = request.args['model']
-    model = model_data[mtype]['model']
-    classes = model_data[mtype]['classes']
-
-    ret = {}
-
-    if 'rev_id' in request.args:
-        text = get_comment(request.args['rev_id'])
-        ret['text'] = text
+    if validation_error:
+        ret = {'error': validation_error}
+    elif request.args['input_type']  == 'rev_id':
+        ret = process_rev_id(request)
     else:
-        text = request.args['text']
+        ret = process_text(request)
     
-    if text:
-        probs = model.predict_proba([text])[0]
-        probs = ['%0.2f' % p for p in probs]
-        ret['p'] = list(zip(classes, probs))
-    
-    t2 = time.time()
-    print('Total Time:', t2-t1)
     return jsonify(ret)
+    
 
 
 @app.after_request
--- app/static/js/score.js
+++ app/static/js/score.js
@@ -1,59 +1,50 @@
 
 
 $('#itype').on('change', function() {
-   $('#model-input').attr("placeholder", $('input[name="itype"]:checked').val()); 
+    var itype = $('input[name="itype"]:checked').val();
+    var pl = 'Comment Text';
+
+    if (itype == 'rev_id')
+        pl = 'Revision ID';
+
+    $('#model-input').attr("placeholder", pl ); 
 });
 
 
 $('#model-submit').click(function () {
 
-        mtype = $('input[name="mtype"]:checked').val()
-        itype = $('input[name="itype"]:checked').val()
+        mtype = $('input[name="mtype"]:checked').val();
+        itype = $('input[name="itype"]:checked').val();
         idata  = encodeURIComponent($('#model-input').val());
+        console.log(itype)
 
-        if (itype == 'Comment Text'){
-
-            $.get(
-                'api?model=' + mtype + '&text=' + idata, // The URL to call
-                function(data) { // Success event handler
-                    if( !('p' in data)) {
-                        $('#model-score').text('ERROR')
-                    }else {
-                        arr = data['p']
-                        res = ''
-                        for(var i=0; i < arr.length; i++){
-                            res += '<p>' + arr[i][0] + ': ' + arr[i][1]
-                        }
-                        $('#model-score').empty()
-                        $('#model-score').append('<p><b>Results: </b></p>')
-                        $('#model-score').append(res)
+        $.get(
+            'api?model=' + mtype + '&input_type=' + itype + '&input=' + idata, // The URL to call
+            function(data) { // Success event handler
+                console.log(idata)
+                if( !('p' in data)) {
+                    if (!('error' in data)){
+                        $('#model-score').text('ERROR: unknown')
+                    }else{
+                        $('#model-score').text(data['error'])
                     }
-                }
-            );
+                    
+                }else {
+                    arr = data['p']
+                    res = ''
+                    for(var i=0; i < arr.length; i++){
+                        res += '<p>' + arr[i][0] + ': ' + arr[i][1]
+                    }
+                    $('#model-score').empty()
 
-        }else{
-
-            $.get(
-                'api?model=' + mtype + '&rev_id=' + idata, // The URL to call
-                function(data) { // Success event handler
-                    if( !('p' in data)) {
-                        $('#model-score').text('ERROR')
-                    }else {
-                        $('#model-score').empty()
+                    if (itype == 'rev_id'){
                         $('#model-score').append('<p><b>Comment: </b></p>')
                         $('#model-score').append('<p>' + data['text'] + '<p>')
-                        arr = data['p']
-                        res = ''
-                        for(var i=0; i < arr.length; i++){
-                            res += '<p>' + arr[i][0] + ': ' + arr[i][1]
-                        }
-                        $('#model-score').append('<p><b>Results: </b></p>')
-                        $('#model-score').append(res)
                     }
+                    $('#model-score').append('<p><b>Results: </b></p>')
+                    $('#model-score').append(res)
                 }
-            );
-        }
-
-
+            }
+        );
 });
 
