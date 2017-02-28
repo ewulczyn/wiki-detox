44021c3f24af33af503adcc5e63e9d165d4213ff
ewulczyn <ellerywulczyn@gmail.com>
2017-02-28 10:39:14 -0800

    moved from user text for user id as the user identifier

--- src/retention/user_object.py
+++ src/retention/user_object.py
@@ -1,12 +1,9 @@
 class User():
-    def __init__(self, user_text, df_annotated_user_text_groups, df_annotated_page_title_groups, df_edits_groups, df_user_groups, df_uw_groups):
-        self.user_text = user_text
-        self.df_activity =  df_edits_groups.get(user_text, None)
-        self.df_comments_made =  df_annotated_user_text_groups.get(user_text, None)
-        self.df_comments_received = df_annotated_page_title_groups.get(user_text, None)
-        self.df_uw = df_uw_groups.get(user_text, None)
-        if self.df_comments_received is not None:
-            self.df_comments_received = self.df_comments_received.query("ns == 'user' and user_text != page_title")
-        self.gender = df_user_groups[user_text]['gender'].iloc[0]
-        self.registration_day = df_user_groups[user_text]['registration_day'].iloc[0]
-        self.first_edit_day = df_user_groups[user_text]['first_edit_day'].iloc[0]
+    def __init__(self, user_id, df_comments_from_groups, df_comments_to_groups, df_edits_groups, df_user_groups, df_uw_groups):
+        self.user_id = user_id
+        self.df_activity =  df_edits_groups.get(user_id, None)
+        self.df_comments_from =  df_comments_from_groups.get(user_id, None)
+        self.df_comments_to = df_comments_to_groups.get(user_id, None)
+        self.df_uw = df_uw_groups.get(user_id, None)
+        self.gender = df_user_groups[user_id]['gender'].iloc[0]
+        self.first_edit_day = df_user_groups[user_id]['first_edit_day'].iloc[0]
